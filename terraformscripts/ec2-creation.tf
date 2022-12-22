7bc458120f630282341aaf2fad7d28903c04df47
rameshm5612 <116630165+rameshm5612@users.noreply.github.com>
2022-12-22 08:13:14 +0530

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -2,7 +2,7 @@ provider "aws"{
    region  = "ap-south-1" 
 }
 
-resource "aws_instance" "AWSEC2Instance"{
+resource "aws_instance" "AWSServer"{
      count   = 1
      ami = "ami-0f9d9a251c1a44858"
      instance_type = "t2.micro"
